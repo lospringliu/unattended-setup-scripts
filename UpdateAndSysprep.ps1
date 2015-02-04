8420291cce70500fa7d6f1c52de39a3bd68d4617
Xinchun Liu <lospringliu@gmail.com>
2015-02-04 15:40:59 +0000

    do not update windows

--- UpdateAndSysprep.ps1
+++ UpdateAndSysprep.ps1
@@ -1,31 +1,15 @@
-Param(
-  [switch]$InstallCloudbaseInit
-)
-$ErrorActionPreference = "Stop"
-
-$Host.UI.RawUI.WindowTitle = "Installing updates..."
-
-Get-WUInstall -AcceptAll -IgnoreReboot
-if (Get-WURebootStatus -Silent)
+Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name Unattend*
+Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoLogonCount
+if($InstallCloudbaseInit)
 {
-    $Host.UI.RawUI.WindowTitle = "Updates installation finished. Rebooting."
-    shutdown /r /t 0
+    Invoke-WebRequest -Uri https://dl.dropboxusercontent.com/u/9060190/InstallCloudbaseInit.ps1 -OutFile C:\Windows\Temp\InstallCloudbaseInit.ps1
+    #C:\Windows\Temp\InstallCloudbaseInit.ps1
+
+    #$Host.UI.RawUI.WindowTitle = "Running Sysprep..."
+    #C:\Windows\System32\Sysprep\Sysprep.exe /generalize /oobe /shutdown /unattend:"C:\Program\ Files\ (x86)\Cloudbase\ Solutions\Cloudbase-Init\conf\Unattend.xml"
 }
 else
 {
-    Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name Unattend*
-    Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoLogonCount
-    if($InstallCloudbaseInit)
-    {
-        Invoke-WebRequest -Uri https://dl.dropboxusercontent.com/u/9060190/InstallCloudbaseInit.ps1 -OutFile C:\Windows\Temp\InstallCloudbaseInit.ps1
-        C:\Windows\Temp\InstallCloudbaseInit.ps1
-
-        #$Host.UI.RawUI.WindowTitle = "Running Sysprep..."
-        #C:\Windows\System32\Sysprep\Sysprep.exe /generalize /oobe /shutdown /unattend:"C:\Program\ Files\ (x86)\Cloudbase\ Solutions\Cloudbase-Init\conf\Unattend.xml"
-    }
-    else
-    {
-        $Host.UI.RawUI.WindowTitle = "Running Sysprep..."      
-        C:\Windows\System32\Sysprep\Sysprep.exe /generalize /oobe /shutdown /unattend:C:\Windows\Temp\Unattend.xml
-    }
+    $Host.UI.RawUI.WindowTitle = "Running Sysprep..."      
+    #C:\Windows\System32\Sysprep\Sysprep.exe /generalize /oobe /shutdown /unattend:C:\Windows\Temp\Unattend.xml
 }
